f7b3532c1e2302184577cf8c48537143486a746b
Federom99 <federo.15.99@gmail.com>
2022-09-08 12:10:31 -0300

    feature

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ const lista = [
     url: "n/a",
   },
 ];
+
+
+let factorial = 1
+for (let i = 10; i > 0; i--) {
+    factorial *= i
+}
+console.log(factorial) 
+
+
+let factorial2 = 1
+let num = 10
+while (num > 1) {
+    factorial *= num
+    num--
+}
+console.log(factorial2)
+
+
+let factorial3 = 1
+let num2 = 10
+while (true) {
+    factorial *= num2
+    num2--
+    if (num2 === 1) break
+}
+console.log(factorial3) 
