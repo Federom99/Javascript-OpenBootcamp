fa1753a4b590e677b9f9cf3204e4822427a865ef
Federom99 <federo.15.99@gmail.com>
2022-09-08 10:55:09 -0300

    change

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const lista = ["federico", 23, true,];
+const lista = [
+  "federico",
+  23,
+  true,
+  new Date(1999, 07, 23),
 
-const fecha = new Date("july 23 1999")
-
-const libro= {
+  {
     titulo: "no tengo",
     autor: "no tengo",
     fecha: "no tengo",
-    url: "n/a"
-}
-
-console.log(lista, fecha, libro)
+    url: "n/a",
+  },
+];
