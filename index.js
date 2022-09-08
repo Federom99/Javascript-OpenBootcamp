3869defa56d58fd2bfcaf41652f7fddcfefad57e
Federom99 <federo.15.99@gmail.com>
2022-09-08 10:38:43 -0300

    feature

---
+++ index.js
@@ -0,0 +1,12 @@
+const lista = ["federico", 23, true,];
+
+const fecha = new Date("july 23 1999")
+
+const libro= {
+    titulo: "no tengo",
+    autor: "no tengo",
+    fecha: "no tengo",
+    url: "n/a"
+}
+
+console.log(lista, fecha, libro)
