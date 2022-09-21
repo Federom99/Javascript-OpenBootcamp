1c696e49301917cd57095abcfcfd99efb126d563
Federom99 <federo.15.99@gmail.com>
2022-09-21 09:53:10 -0300

    feature

--- index.js
+++ index.js
@@ -157,3 +157,27 @@ function* ids() {
       yield id += 2
   }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+import winston from 'winston';
+
+
+const logger = winston.createLogger({
+  level: "error",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+  ],
+});
+
+function showError() {
+  throw new Error("showError function");
+}
+
+try {
+  showError();
+} catch (e) {
+  logger.log("error", e.toString());
+}
